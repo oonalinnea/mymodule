4c2b41d4dcaa9c9459cef6acd97e8270de2288ac
Oona Linnea <oona.lahteenmaki@myy.haaga-helia.fi>
2016-12-11 19:23:21 +0000

    Update

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,32 @@
 class omamoduuli {
-	package {apache2:
-		ensure => "installed",
-		allowcdrom => "true",
-	}
- 
-	package {libreoffice:
-		ensure => "installed",
-		allowcdrom => "true",
+	Package {ensure => "installed", allowcdrom => "true",}
+	package {"apache2":}
+	package {"php7.0":}
+	package {"gedit":}
+	package {"libreoffice":}
+	package {"ssh":}
+	package {"git":}
+	package {"mysql-client":}
+	package {"mysql-server":}
+	package {"libapache2-mod-php":
+		require => Package ["apache2"],}
+
+	file { "/etc/motd":
+		ensure => 'link',
+		target => "/var/run/motd",	
 	}
 
-	package {gedit:
-		ensure => "installed",
-		allowcdrom => "true",
+	file { "/var/run/motd":
+		ensure => present,
+		content => "Tervetuloa!\n",
 	}
 
 	file { "/etc/skel/public_html":
 		ensure => "directory",
+	}
+
+	file { "/etc/skel/public_html/index.php":
+		content => template("omamoduuli/index.php"),
 	}
 
 	file { '/etc/apache2/mods-enabled/userdir.conf':
@@ -30,6 +41,11 @@ class omamoduuli {
 		target => '../mods-available/userdir.load',
 		notify => Service["apache2"],
 		require => Package["apache2"],
+	}
+
+	file {"/etc/apache2/mods-available/php7.0.conf":
+		content => template("omamoduuli/php7.0.conf"),
+		notify => Service ["apache2"],
 	}
 
 	service {"apache2":
